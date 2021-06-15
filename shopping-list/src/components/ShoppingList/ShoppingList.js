72bb148c96827f7ad673cf41684dbba9ae2f40ce
Paige Godfrey <paagodfrey@gmail.com>
2021-06-15 05:45:39 -0700

    stripped out logic for start

--- shopping-list/src/components/ShoppingList/ShoppingList.js
+++ shopping-list/src/components/ShoppingList/ShoppingList.js
@@ -1,22 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 import ListItemForm from "../ListItemForm/ListItemForm";
-// import ListItem from "../ListItem/ListItem";
+import ListItem from "../ListItem/ListItem";
 import './ShoppingList.css'
-import { v4 as uuid } from 'uuid';
 
 export default function ShoppingList() {
-  const [items, setItems] = useState([]);
-
-  const addItem = item => {
-    let newItem = { ...item, id: uuid() };
-    setItems(items => [...items, newItem]);
-  };
-
   return (
-    <div className={`ShoppingList ${items.length ? "list-filled" : "list-empty"}`}>
+    <div className="ShoppingList">
       <h1>Shopping List</h1>
-      <ListItemForm addItem={addItem} />
+      <ListItemForm />
       {/* map over ListItem */}
+      <ListItem />
       {/* if no items show message */}
     </div>
   );
